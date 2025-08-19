76e978842990db1c575931fe3bc3ea55d75574db
tw00n <shiningtwoon@gmail.com>
2025-08-19 16:35:15 +0900

    send FormData type request body that wrap image file to FastAPI

--- src/img-scaleup/img-scaleup.service.ts
+++ src/img-scaleup/img-scaleup.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import FormData from 'form-data';
 
 
 @Injectable()
@@ -10,8 +11,11 @@ export class ImgScaleupService {
   constructor(private readonly httpService: HttpService) {}
 
   async uploadImage(image: Express.Multer.File) {
+    const form = new FormData();
+    form.append("image", image.buffer);
+
     const { data } = await firstValueFrom(
-      this.httpService.post(`${this.baseUrl}/upload`, image),
+      this.httpService.post(`${this.baseUrl}/upload`, form),
     );
     return data;
   }
