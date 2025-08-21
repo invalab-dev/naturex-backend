49d0c85859fe651328c255f54c8acfae64bd5d99
tw00n <shiningtwoon@gmail.com>
2025-08-21 10:53:22 +0900

    add ConfigModule because one of default behaviour of nest.js can not read .env; just only use environment values defined in OS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImgScaleupModule } from './img-scaleup/img-scaleup.module';
 import { GlobalModule } from './global.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ImgScaleupModule, GlobalModule],
+  imports: [ImgScaleupModule, GlobalModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
