57175d757bcb95580727d5e69c54ce4b6e5a3255
tw00n <shiningtwoon@gmail.com>
2026-02-19 10:31:16 +0900

    delete old map

--- src/map/area-groups/area-groups.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AreaGroupsController } from './area-groups.controller.js';
-import { AreaGroupsService } from './area-groups.service.js';
-
-@Module({
-  controllers: [AreaGroupsController],
-  providers: [AreaGroupsService],
-})
-export class AreaGroupsModule {}
